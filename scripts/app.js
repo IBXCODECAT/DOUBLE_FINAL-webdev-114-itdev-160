157e84d5ef6b4c52f12c9def4dc0cd2b9bf16057
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-05-08 15:51:25 -0500

    Make Code Beautiful

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,7 @@
-function AddBlueBot()
-{
+function AddBlueBot() {
     const response = confirm("You are about to be redirected outside Blue Bot's website.");
 
-    if(response)
-    {
+    if (response) {
         window.open('https://canary.discord.com/api/oauth2/authorize?client_id=988833332445978624&permissions=536870944&scope=bot%20applications.commands', '_blank');
     }
 }
--- scripts/php-check.js
+++ scripts/php-check.js
@@ -1,6 +1,5 @@
 
-function CheckDomainOrHostLocation()
-{
+function CheckDomainOrHostLocation() {
     //
     const staticHosts = ['file://', 'localhost', 'github.io', 'github.com', 'bluescreenstudios.net']
     const host = window.location.href;
@@ -8,20 +7,17 @@ function CheckDomainOrHostLocation()
 
     //Loop through each static hosts and check the current window location for that substring
 
-    for(let i = 0; i < staticHosts.length; i++)
-    {
-        if(host.includes(staticHosts[i]))
-        {
+    for (let i = 0; i < staticHosts.length; i++) {
+        if (host.includes(staticHosts[i])) {
             validPHPHost = false;
         }
     }
 
     //If no match was found we have a valid PHP host
-    if(validPHPHost == null) validPHPHost = true;
+    if (validPHPHost == null) validPHPHost = true;
 
     //If our server does not support PHP, let's remove our PHP contact form and add an error message
-    if(!validPHPHost)
-    {
+    if (!validPHPHost) {
         document.getElementById('php_invalid').textContent = "This deployment does not support server side scripting so we couldn't load a contact form for you. Don't panic though! You can still click the elephent bellow to create a draft in your favorite email client.";
         document.getElementById('contact_form').remove();
 
@@ -32,7 +28,7 @@ function CheckDomainOrHostLocation()
         img.style.height = 10 + "%";
         img.style.width = 10 + "%";
         img.id = "php_animal";
-        
+
         img.addEventListener('click', (e) => {
             console.log("Elephent Clicked!");
             window.open("mailto:contact@bluescreenstudios.net");
